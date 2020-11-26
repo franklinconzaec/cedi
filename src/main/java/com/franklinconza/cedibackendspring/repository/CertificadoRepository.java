4dd8b06d41648cc9fe7567c0d3bf4e63a7745ec6
Franklin Conza-Apolo <me@franklinconza.com>
2020-11-26 14:48:57 -0500

    Update CertificadoRepository.java

--- src/main/java/com/franklinconza/cedibackendspring/repository/CertificadoRepository.java
+++ src/main/java/com/franklinconza/cedibackendspring/repository/CertificadoRepository.java
@@ -11,8 +11,7 @@ import java.util.List;
 @Repository
 public interface CertificadoRepository extends JpaRepository<Certificado, String> {
 
-    @Query(value = "select case when rol.id='PONE' then 'I'||ins.id||'_P'||pon.id when rol.id='CONF' then 'I'||ins.id||'_C'||pon.id " +
-            "when rol.id='INVE' then 'I'||ins.id||'_D'||pon.id else 'I'||ins.id end as id, " +
+    @Query(value = "select case when pon.id is not then 'I'||ins.id||'_T'||pon.id else 'I'||ins.id end as id, " +
             "eve.id as eventoid, eve.nombre as evento, rol.id as rolid, rol.descripcion as rol, ins.nombre as participante, pon.tema as tema " +
             "from certificados.inscripciones ins " +
             "inner join certificados.eventos eve on (eve.id=ins.evento) " +
